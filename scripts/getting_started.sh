fe4d6eba9c6b9f87dc0ca4886ab5f2f91b45577a
malctaylor15 <malctaylor15@gmail.com>
2020-02-15 13:02:14 -0500

    add columns if api schema changes, fix back fill, update getting started

--- scripts/getting_started.sh
+++ scripts/getting_started.sh
@@ -5,6 +5,7 @@ source meh_scraper_env/bin/activate
 pip install -r scripts/requirements.txt
 mkdir notebooks/run_notebooks
 mkdir data
+sqlite3 data/meh_scraper.db < create_db.sh
 
 # Optional -- create SQLite3 Databases
 # bash create_db.sh
@@ -15,4 +16,5 @@ mkdir data
 # Run the notebook using papermill
 bash scripts/run_notebook.sh
 58 23 * * * bash /home/pi/meh_scraper/scripts/run_notebook.sh
+53 */3 * * * bash /home/pi/meh_scraper/scripts/run_site_scraper.sh
 
