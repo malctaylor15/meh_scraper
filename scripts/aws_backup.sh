5f9e5447b095b1e0707a714931ee8742a0b9780e
malctaylor15 <malctaylor15@gmail.com>
2021-07-15 13:51:40 -0400

    add generalized email class notebook

--- scripts/aws_backup.sh
+++ scripts/aws_backup.sh
@@ -2,7 +2,8 @@ export path1="$(dirname "$0")"
 cd $path1
 cd ..
 echo "current working directory: "$PWD
+export aws="/usr/local/bin/aws"
 
 DATE=`date +%m-%d-%y`
 new_file_name=meh_backup_${DATE}.db
-aws s3 cp data/meh_scraper.db s3://do-mt-backups/meh_db_backups/${new_file_name}
+$aws s3 cp data/meh_scraper.db s3://do-mt-backups/meh_db_backups/${new_file_name}
